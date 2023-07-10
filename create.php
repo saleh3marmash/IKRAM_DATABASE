9488c7f754edc0083d97f6f40f29c2004ab12f30
saleh3marmash <1193300@student.birzeit.edu>
2023-07-11 01:42:21 +0300

    first commit

---
+++ create.php
@@ -0,0 +1,33 @@
+<?php
+try{
+    $con=mysqli_connect('localhost','root','AaAa*2461949');
+    if($con){
+        $sql = "CREATE DATABASE IF NOT EXISTS hotels";
+        if ($con->query($sql) === TRUE) {
+            echo "Database created succefully";
+        }else{
+            echo "Database creation failed";
+        }
+        $sql = "use hotels";
+        $con->query($sql);
+        $sql = "CREATE TABLE IF NOT EXISTS users (
+            user_id INT AUTO_INCREMENT PRIMARY KEY,
+            user_password VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            age INT NOT NULL
+        )";
+        // Execute the query
+        if ($con->query($sql) === TRUE) {
+            echo "Table created successfully";
+        } else {
+            echo "Error creating table: " . $con->error;
+        }
+        mysqli_close($con);
+    }
+    else{
+        echo "connection failed";
+        echo "Failure";
+    }
+}catch(Error $e){
+    echo($e->getMessage());
+}
