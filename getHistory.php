b083af3d807c1c9b1c622bc72cd580f51ef9ff93
otaki00 <omarbin382@gmail.com>
2023-07-11 09:32:16 +0300

    om

---
+++ getHistory.php
@@ -0,0 +1,37 @@
+<?php
+    
+
+    $user_id = "";
+    
+    if(isset($_GET['user_id'])){
+        $user_id = $_GET['user_id'];
+    }
+
+    if(!empty($user_id)){
+        $serverName = "localhost";
+        $userName = "root";
+        $password = "omar404";
+        $dbname ="hotels";
+
+        $conn = new mysqli($serverName, $userName, $password, $dbname);
+
+        if($conn-> connect_error){
+            die("Connection feild: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT * FROM users WHERE user_id = '".$user_id."'";
+
+        $result = $conn->query($sql);
+        $resultArray = array();
+
+        while($row = mysqli_fetch_assoc($result)){
+            $resultArray[] = $row;
+        }
+
+        echo json_encode($resultArray);
+
+        $conn->close();
+
+    }
+
+?>
